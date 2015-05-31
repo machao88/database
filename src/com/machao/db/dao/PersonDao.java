421ed4d36982785dbdba1dd98af388dde2e08b91
machao <machao@home.(none)>
2015-05-31 12:46:15 +0800

    [db:add,high]add DB add,modify,delete,find.

---
+++ src/com/machao/db/dao/PersonDao.java
@@ -0,0 +1,60 @@
+package com.machao.db.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.machao.db.PersonSQLiteOpenHelper;
+import com.machao.db.domain.Person;
+
+public class PersonDao {
+	private PersonSQLiteOpenHelper helper;
+	
+
+
+	public  PersonDao(Context context){
+		helper = new PersonSQLiteOpenHelper(context);
+	}
+
+	public void add(String name, String number){
+		SQLiteDatabase db = helper.getWritableDatabase();
+		db.execSQL("insert into person (name, number) values (?, ?)", new Object[]{name, number});
+		db.close();
+	}
+	public void update(String name, String newnumber){
+		SQLiteDatabase db = helper.getWritableDatabase();
+		db.execSQL("update person set number=? where name = ?;", new Object[]{newnumber, name});
+		db.close();
+	}
+	public void delete(String name){
+		SQLiteDatabase db = helper.getWritableDatabase();
+		db.execSQL("delete from person where name = ?", new Object[]{name});
+		db.close();
+	}
+	public boolean find(String name){
+		SQLiteDatabase db = helper.getWritableDatabase();
+		Cursor cursor = db.rawQuery("select * from person where name =?;", new String[]{name});
+		boolean b = cursor.moveToNext();
+		cursor.close();
+		db.close();
+		return b;
+	}
+	public List<Person> findAll(){
+		List<Person> persons = new ArrayList<Person>();
+		SQLiteDatabase db = helper.getWritableDatabase();
+		Cursor cursor = db.rawQuery("select * from person;", null);
+		while(cursor.moveToNext()){
+			int id = cursor.getInt(cursor.getColumnIndex("id"));
+			String name = cursor.getString(cursor.getColumnIndex("name"));
+			String number = cursor.getString(cursor.getColumnIndex("number"));
+			Person person = new Person(id, name, number);
+			persons.add(person);
+		}
+		cursor.close();
+		db.close();
+		return persons;
+	}
+}
--- src/com/machao/db/test/TestPersonDB.java
+++ src/com/machao/db/test/TestPersonDB.java
@@ -1,6 +1,10 @@
 package com.machao.db.test;
 
+import java.util.List;
+
 import com.machao.db.PersonSQLiteOpenHelper;
+import com.machao.db.dao.PersonDao;
+import com.machao.db.domain.Person;
 
 import android.test.AndroidTestCase;
 
@@ -10,4 +14,30 @@ public class TestPersonDB extends AndroidTestCase {
 		PersonSQLiteOpenHelper helper = new PersonSQLiteOpenHelper(getContext());
 		helper.getWritableDatabase();
 	}
+	public void add() {
+		PersonDao pd = new PersonDao(getContext());
+		pd.add("lisi", "1234");
+	}
+
+	public void update()throws Exception {
+		PersonDao pd = new PersonDao(getContext());
+		pd.update("lisi", "12345678");
+	}
+	public void delete()throws Exception {
+		PersonDao pd = new PersonDao(getContext());
+		pd.delete("lisi");
+	}
+	public void find() throws Exception{
+		PersonDao pd = new PersonDao(getContext());
+		boolean result = pd.find("lisi");
+		assertEquals(true, result);
+	}
+	public void findAll() throws Exception{
+		PersonDao pd = new PersonDao(getContext());
+		List<Person> persons = pd.findAll();
+		for(Person p:persons){
+			System.out.println(p.toString()+ "  "+ p.getId()+ p.getName() + p.getNumber());
+		}
+	}
 }
+
